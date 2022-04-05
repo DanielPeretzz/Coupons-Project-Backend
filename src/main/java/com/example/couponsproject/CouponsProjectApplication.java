48231b40dd6f1a1ab30c81b838981c648bc47d56
DanielPeretzz <93336006+DanielPeretzz@users.noreply.github.com>
2022-04-06 00:38:20 +0300

    Initial commit

--- src/main/java/com/example/couponsproject/CouponsProjectApplication.java
+++ src/main/java/com/example/couponsproject/CouponsProjectApplication.java
@@ -1,13 +1,19 @@
 package com.example.couponsproject;
 
+import com.example.couponsproject.beans.Company;
+import com.example.couponsproject.repository.CompanyRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @SpringBootApplication
 public class CouponsProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CouponsProjectApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(CouponsProjectApplication.class, args);
+		CompanyRepository companyRepository = applicationContext.getBean(CompanyRepository.class);
+		companyRepository.save(new Company("daniel@gmail.com","iNNpAx","147147"));
 	}
 
 }
